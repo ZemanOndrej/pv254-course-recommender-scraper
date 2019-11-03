750aad0a11f4a73120e35b9b1a955082f46aed12
Ondrej Zeman <ozeman77@gmail.com>
2019-11-03 12:48:50 +0100

    indent-4 json

--- src/util.py
+++ src/util.py
@@ -8,7 +8,7 @@ def saveJson(json_output, filename='courses.json'):
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
     with open(filename, 'w+', encoding="utf-8") as f:
-        json.dump(json_output, f)
+        json.dump(json_output, f,indent=4)
 
 
 def get_subject_urls(subject):
